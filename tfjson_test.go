49539f8bfdef9ec7c5fac4ae862fd7c87d7de0af
Andrew Partis <tapirs-technology@outlook.com>
2019-04-12 13:23:38 +0100

    removed init

--- tfjson_test.go
+++ tfjson_test.go
@@ -121,7 +121,7 @@ func Test(t *testing.T) {
 
 	planPath := filepath.Join(dir, "terraform.tfplan")
 	mustRun(t, "terraform", "get", dir)
-	mustRun(t, "terraform", "init", dir)
+	//mustRun(t, "terraform", "init", dir)
 	mustRun(t, "terraform", "plan", "-out="+planPath, dir)
 
 	j, err := tfjson(planPath)
